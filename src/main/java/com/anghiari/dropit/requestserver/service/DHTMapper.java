61577dda9c2f4fde4b33ac1b97d0d76cfa6c5a18
Rajith Siriwardana <rajithsiriwardana@gmail.com>
2014-01-20 22:52:07 +0530

    interface defined

---
+++ src/main/java/com/anghiari/dropit/requestserver/service/DHTMapper.java
@@ -0,0 +1,13 @@
+package com.anghiari.dropit.requestserver.service;
+
+/**
+ * @author rajith
+ * @version ${Revision}
+ */
+public class DHTMapper implements Mapper{
+
+    @Override
+    public String calculateNodeId() {
+        return null;
+    }
+}
---
+++ src/main/java/com/anghiari/dropit/requestserver/service/Mapper.java
@@ -0,0 +1,10 @@
+package com.anghiari.dropit.requestserver.service;
+
+/**
+ * @author rajith
+ * @version ${Revision}
+ */
+public interface Mapper {
+
+    public String calculateNodeId();
+}
