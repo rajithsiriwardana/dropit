ae3b47a2e297c256a99b3161eae6c2ab0e04be62
Gayashan Amarasinghe <chozrc@gmail.com>
2014-01-21 17:40:42 +0530

    Hash generation is a static function.

--- src/main/java/com/anghiari/dropit/requestserver/service/DHTMapper.java
+++ src/main/java/com/anghiari/dropit/requestserver/service/DHTMapper.java
@@ -7,14 +7,13 @@ import java.security.NoSuchAlgorithmException;
  * @author rajith
  * @version ${Revision}
  */
-public class DHTMapper implements Mapper {
-    private final int KEYSPACE = 32;
+public class DHTMapper {
+    private static final int KEYSPACE = 32;    //keyspace = 32 , should be enough
 
-    @Override
-    public KeyId generateKeyId(String key) {
+    public static KeyId generateKeyId(String key) {
         try {
             KeyId keyId = new KeyId(0);
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
             byte[] hash = messageDigest.digest(key.getBytes());
             int keySpaceBytes = KEYSPACE / 4;
             for (int i = 0; i < Math.min(hash.length, keySpaceBytes); ++i) {
