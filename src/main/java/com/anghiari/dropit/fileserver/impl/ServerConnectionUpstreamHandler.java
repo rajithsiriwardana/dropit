aa4132a65eb629a66f7da3b5fd05509efe6b3474
chinthaka316 <chinthakadd@gmail.com>
2014-01-21 08:21:10 +0530

    adding new main method and upstream handler

---
+++ src/main/java/com/anghiari/dropit/fileserver/impl/ServerConnectionUpstreamHandler.java
@@ -0,0 +1,16 @@
+package com.anghiari.dropit.fileserver.impl;
+
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+
+
+/**
+ * @author: chinthaka
+ * This is the place to implement the protocol between the client(Request Server) and node in recieving a file
+ */
+
+public class ServerConnectionUpstreamHandler extends SimpleChannelUpstreamHandler{
+
+	//private final ServerListener listener;
+	
+	
+}
