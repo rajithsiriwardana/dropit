a909fe9517c43927eac214ba21923aca355c4fd6
amila03ac <amila03ac@gmail.com>
2014-01-24 01:48:31 +0530

    added Configurations

---
+++ src/main/java/com/anghiari/dropit/commons/Configurations.java
@@ -0,0 +1,11 @@
+package com.anghiari.dropit.commons;
+
+/**
+ * User: amila
+ */
+public class Configurations {
+    public final static int[] extPorts = {14500,14501,14502,14503,14504};
+    public final static int[] intPorts = {15500,15501,15502,15503,15504};
+
+    public final static int[] fileNodeKeys = {100, 200, 300, 400, 500};
+}
--- src/main/java/com/anghiari/dropit/execute/FileServerRunner.java
+++ src/main/java/com/anghiari/dropit/execute/FileServerRunner.java
@@ -1,5 +1,6 @@
 package com.anghiari.dropit.execute;
 
+import com.anghiari.dropit.commons.Configurations;
 import com.anghiari.dropit.commons.FileNode;
 import com.anghiari.dropit.commons.KeyId;
 import com.anghiari.dropit.fileserver.impl.FileServerNodeImpl;
@@ -15,21 +16,22 @@ public class FileServerRunner {
     public static void main(String[] args) {
 
         String ip = "127.0.0.1";
-        int port = 14500;
-        int port_ring = 15500;
-        int key = 100;
-        int numberOfNodes = Integer.parseInt(args[0]);
+//        int numberOfNodes = Integer.parseInt(args[0]);
+        int numberOfNodes = 5;
+        int[] intPorts = Configurations.intPorts;
+        int[] extPorts = Configurations.extPorts;
+        int[] keys = Configurations.fileNodeKeys;
 
         FileNode node;
         FileServerNodeImpl fileServer;
 
         for(int i = 0; i < numberOfNodes; i++){
-            node = new FileNode(ip, port++, port_ring++, new KeyId(key++));
+            node = new FileNode(ip, extPorts[i], intPorts[i], new KeyId(keys[i]));
             fileServer = new FileServerNodeImpl();
             fileServer.bootServer(node);
         }
 
-        PingOperation op=new PingOperation(new FileServerNodeImpl(),ip,--port);
+        PingOperation op=new PingOperation(new FileServerNodeImpl(),ip, 14501);
         op.sendRequest();
 
         
