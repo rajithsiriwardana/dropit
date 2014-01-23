6e8dc18e3403bf4b68d51a1be6c5d98f8a2b94dd
chinthaka316 <chinthakadd@gmail.com>
2014-01-24 00:54:09 +0530

    adding RingCommunicationHandler

---
+++ src/main/java/com/anghiari/dropit/fileserver/impl/RingCommunicationHandler.java
@@ -0,0 +1,40 @@
+package com.anghiari.dropit.fileserver.impl;
+
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+import com.anghiari.dropit.commons.Constants;
+import com.anghiari.dropit.commons.DropItPacket;
+import com.anghiari.dropit.operations.PongOperation;
+
+public class RingCommunicationHandler extends SimpleChannelHandler {
+
+
+	public void messageReceived(ChannelHandlerContext ctx,MessageEvent e) throws Exception {
+        DropItPacket pkt = (DropItPacket)e.getMessage();
+        String method =pkt.getMethod();
+        
+        if(Constants.PING.toString().equalsIgnoreCase(method)){
+        	System.out.println("RingCommunicationHandler came here "+method );
+            PongOperation pongOperation=new PongOperation(ctx,e);
+            pongOperation.sendResponse();
+        	//respondToPing(ctx,e);
+        }
+        super.messageReceived(ctx, e);
+    }
+	
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
+    		throws Exception {
+    	
+    	//handle successor
+    	System.out.println("Connection exception, server might be down");
+    	Channels.close(e.getChannel());
+    }
+    
+    
+}
