05028513d1a3d6e4cee3f685396cd7768ca3d3b4
Gayashan Amarasinghe <chozrc@gmail.com>
2014-01-25 01:37:32 +0530

    Constants updated. REQ_JOIN_NODE added.

--- src/main/java/com/anghiari/dropit/commons/Constants.java
+++ src/main/java/com/anghiari/dropit/commons/Constants.java
@@ -5,20 +5,22 @@ package com.anghiari.dropit.commons;
  */
 public enum Constants {
 
-	/* Communication protocol methods */
-	GET, RES_GET, PUT, RES_PUT, STORE, ACK_STORE, RETRIEVE, TRANSFER, PING, PONG, FND_SUSC, RES_SUSC, GOSSIP, GET_FILENODE, RES_GET_FILENODE, REQ_JOIN_NODE, RES_JOIN_NODE,
+    /*Communication protocol methods*/
+    REQ, RES, GET, RES_GET, PUT, RES_PUT, STORE, ACK_STORE, RETRIEVE, TRANSFER, PING, PONG, FND_SUSC, RES_SUSC, GOSSIP, GET_FILENODE,
+    RES_GET_FILENODE, FILE_PATH, REQ_JOIN_NODE, RES_JOIN_NODE,
 
-	/* Attribute keys */
-	FILE_NAME, KEY_ID, GOS_LIST, INET_ADDRESS, NODE_IP, NODE_PORT;
 
-	public static final int KEY_SPACE = 4;
-	public static final int SUCCESSOR_LIST_SIZE = 3;
+    /* Attribute keys */
+    FILE_NAME, KEY_ID, GOS_LIST, INET_ADDRESS, NODE_IP, NODE_PORT;
 
-	public static final FileNode[] REQUEST_SERVER_LIST = new FileNode[2];
+    public static final int KEY_SPACE = 4;
+    public static final int SUCCESSOR_LIST_SIZE = 3;
 
-	static {
-		REQUEST_SERVER_LIST[0] = new FileNode("", 0);
-		REQUEST_SERVER_LIST[1] = new FileNode("", 0);
-	}
+    public static final FileNode[] REQUEST_SERVER_LIST = new FileNode[2];
+
+    static {
+        REQUEST_SERVER_LIST[0] = new FileNode("", 0);
+        REQUEST_SERVER_LIST[1] = new FileNode("", 0);
+    }
 
 }
