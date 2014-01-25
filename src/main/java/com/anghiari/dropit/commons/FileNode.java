3775d55d8434d488f34ade86e0655344a0b6b80f
chinthaka316 <chinthakadd@gmail.com>
2014-01-25 11:46:20 +0530

    minor changes

--- src/main/java/com/anghiari/dropit/commons/FileNode.java
+++ src/main/java/com/anghiari/dropit/commons/FileNode.java
@@ -1,11 +1,20 @@
 package com.anghiari.dropit.commons;
 
+import java.util.ArrayList;
+
 public class FileNode {
 
+	private int id;
     private String ip;
     private int port;
     private int port_ring;
     private KeyId key;
+    
+    
+//    private FileNode successor;
+//    private ArrayList<FileNode> successors;
+//    private FileNode predeccesor;
+    
 
     public FileNode(String ip, int port){
         this.ip = ip;
@@ -13,6 +22,14 @@ public class FileNode {
     }
 
     public FileNode(String ip, int port, int port_ring, KeyId key){
+        this.ip = ip;
+        this.port = port;
+        this.port_ring = port_ring;
+        this.key = key;
+    }
+    
+    public FileNode(int id,String ip, int port, int port_ring, KeyId key){
+    	this.setId(id);
         this.ip = ip;
         this.port = port;
         this.port_ring = port_ring;
@@ -61,4 +78,36 @@ public class FileNode {
     public int getPort_ring() {
         return port_ring;
     }
+
+	public FileNode getSuccessor() {
+		return successor;
+	}
+
+	public void setSuccessor(FileNode successor) {
+		this.successor = successor;
+	}
+
+	public ArrayList<FileNode> getSuccessors() {
+		return successors;
+	}
+
+	public void setSuccessors(ArrayList<FileNode> successors) {
+		this.successors = successors;
+	}
+
+	public FileNode getPredeccesor() {
+		return predeccesor;
+	}
+
+	public void setPredeccesor(FileNode predeccesor) {
+		this.predeccesor = predeccesor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
