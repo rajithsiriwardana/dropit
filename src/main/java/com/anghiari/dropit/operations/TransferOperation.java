c1109284cac8cbb9b68b7359f95a47432f1be582
Rajith Siriwardana <rajithsiriwardana@gmail.com>
2014-01-25 11:40:26 +0530

    check file exists bug fixed

--- src/main/java/com/anghiari/dropit/operations/TransferOperation.java
+++ src/main/java/com/anghiari/dropit/operations/TransferOperation.java
@@ -12,11 +12,11 @@ public class TransferOperation extends AbstractOperation {
     public TransferOperation(ChannelHandlerContext channelHandlerContext, MessageEvent evt, byte[] filedata, String filename) {
         packet = new DropItPacket(Constants.TRANSFER.toString());
         packet.setAttribute(Constants.FILE_NAME.toString(), filename);
-        if(filedata!=null){
+        if(filedata.length > 0){
             packet.setData(filedata);
-            packet.setAttribute(Constants.FILE_EXISTS.toString(), true);
+            packet.setAttribute(Constants.FILE_EXISTS.toString(), "TRUE");
         } else {
-            packet.setAttribute(Constants.FILE_EXISTS.toString(), false);
+            packet.setAttribute(Constants.FILE_EXISTS.toString(), "FALSE");
         }
         ctx = channelHandlerContext;
         e = evt;
