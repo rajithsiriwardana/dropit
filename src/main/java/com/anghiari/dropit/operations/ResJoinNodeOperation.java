7978949be5ffda86ab1a71ede67eca17c0bbe41c
Sanka Darshana <sankadarshana@gmail.com>
2014-01-25 08:36:44 +0530

    Added new constants for neigbor handling

--- src/main/java/com/anghiari/dropit/operations/ResJoinNodeOperation.java
+++ src/main/java/com/anghiari/dropit/operations/ResJoinNodeOperation.java
@@ -5,7 +5,7 @@ import com.anghiari.dropit.commons.DropItPacket;
 public class ResJoinNodeOperation extends AbstractOperation {
 
 	public ResJoinNodeOperation(DropItPacket pkt) {
-		// TODO Auto-generated constructor stub
+
 	}
 
 }
